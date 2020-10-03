f101d7dd0fcfd49ec61e6e497bed1605c3e87e11
lIIllIIlIlI <43790987+lIIllIIlIlI@users.noreply.github.com>
2020-10-03 12:42:38 +0200

    Update train.py

---
+++ src/Commute/train.py
@@ -0,0 +1,13 @@
+# Web scraping can be done by a wide range of python modules, with the most common one beeing 'beautifull soup'.
+# Web automation is done almost exclusively by the 'Selenium' python package that already comes with a scraper.
+# Web automation always requires both a browser and an additional driver tool. The driver tool can be a 
+# browser plugin, a python module or a local installation. 
+# Some of the tutorials use firefox, yet most of them prefer chromium.
+# 
+#   Choosen automation setup: Chromium + chromium-chromedriver
+#
+#   install: https://www.youtube.com/watch?v=4TuawlLrWaY
+#   usage: https://www.youtube.com/watch?v=QPp0UrefaZo
+#   run headless: https://stackoverflow.com/questions/46753393/how-to-make-firefox-headless-programmatically-in-selenium-with-python
+#
+#   Website to scrape: 'https://reiseauskunft.bahn.de/bin/query.exe/dn?protocol=https:'
