5dea156bfdc22c4e9206698f27961b7bcf21fa5b
lIIllIIlIlI <43790987+lIIllIIlIlI@users.noreply.github.com>
2020-10-03 13:30:01 +0200

    Update car.py

---
+++ src/Commute/car.py
@@ -0,0 +1,28 @@
+# stolen code from 'kiteco/python-youtube-code' 
+
+import requests
+import smtplib 
+
+# API key
+api_file = open("api-key.txt", "r")
+api_key = api_file.readline()
+api_file.close()
+
+# home address input
+home = input("Enter a home address\n") 
+  
+# work address input
+work = input("Enter a work address\n") 
+  
+# base url
+url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&"
+
+# get response
+r = requests.get(url + "origins=" + home + "&destinations=" + work + "&key=" + api_key) 
+ 
+# return time as text and as seconds
+time = r.json()["rows"][0]["elements"][0]["duration"]["text"]       
+seconds = r.json()["rows"][0]["elements"][0]["duration"]["value"]
+  
+# print the travel time
+print("\nThe total travel time from home to work is", time)
